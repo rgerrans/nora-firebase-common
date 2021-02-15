7cbe4982ddbdd6a83cdd568aa84ac7aad4bd500b
rgerrans <rgerrans@gmail.com>
2021-02-15 10:15:37 -0700

    Update device.ts

--- src/device.ts
+++ src/device.ts
@@ -23,9 +23,14 @@ export interface Device<TState = {}, TAttributes = {}, TNora = {}> {
     willReportState: boolean;
     notificationSupportedByAgent?: boolean;
     /**
-    * @maxLength 40
-    */
+     * @description Name of the home this device belongs to
+     * @maxLength 40
+     */
     structureHint?: string;
+    /**
+     * @description Name of the room this device belongs to
+     * @maxLength 40
+     */
     roomHint?: string;
     deviceInfo?: {
         manufacturer: string;
@@ -192,9 +197,20 @@ type DeviceType =
     'action.devices.types.OUTLET' |
     'action.devices.types.THERMOSTAT' |
     'action.devices.types.SPEAKER' |
+    'action.devices.types.LOCK' |
+
+    'action.devices.types.CLOSET' |
+    'action.devices.types.AWNING' |
+    'action.devices.types.CURTAIN' |
+    'action.devices.types.DOOR' |
+    'action.devices.types.DRAWER' |
     'action.devices.types.BLINDS' |
     'action.devices.types.GARAGE' |
-    'action.devices.types.LOCK';
+    'action.devices.types.GATE' |
+    'action.devices.types.PERGOLA' |
+    'action.devices.types.SHUTTER' |
+    'action.devices.types.VALVE';
+    'action.devices.types.WINDOW';
 
 export type Trait =
     'action.devices.traits.Brightness' |
